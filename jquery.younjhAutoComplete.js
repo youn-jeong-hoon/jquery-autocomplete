0d70e12a9e44780ebb8f850ea71cb5a27cab4ca0
youn-jeong-hoon <yjh5369@hanmail.net>
2012-08-27 16:30:18 +0900

    bug 수정
    2개 이상일때 제대로 동작하지 않는 버그 수정

--- jquery.younjhAutoComplete.js
+++ jquery.younjhAutoComplete.js
@@ -7,8 +7,30 @@
 (function ($){
 	$.fn.younjhAutoComplete = function(options){
 		var $this = $(this);
+		var option = {
+				width: 151,
+				height: 20,
+				background: 'white',
+				border: '2px solid black',
+				fontSize: 11,
+				fontFamily: 'Gulim, Dotum, Tahoma, Sans-serif',
+				fontColor: 'black',
+				list: {
+					background: 'white',
+					border: '1px solid black',
+					zIndex: 100000,
+					limit: 10,
+					showRows: 10
+				},
+				button: true
+		};
+
+		// option setting
+		$.extend(true, option, options);
+
+//		return $(this).each(function(){
 		var $item = $this.children();
-		var $container = $this.wrap('<div id="younjh_container"></div>').parent();
+		var $container = $this.wrap('<div class="younjh_container"></div>').parent();
 		var start;				// 왜 검색 되었는지 검색어와 일치하는 부분에 highlight를 주기 위한 index 번호 
 		var result = "";		// 검색 결과 저장 변수
 		var $younjh_ul;			// 검색 목록 객체
@@ -21,42 +43,32 @@
 		var en = [];			// option value encode array
 		var enIdx = [];			// 인코딩 된 text로 value를 얻기 위해 en과 같은 index에 value를 저장한다.
 		var resultId = [];		// 검색된 결과의 value 목록
-		var option = {
-				width: 151,
-				height: 20,
-				background: 'white',
-				border: '2px solid black',
-				fontSize: 10,// font-size
-				fontFamily: 'Gulim, Dotum, Tahoma, Sans-serif',
-				fontColor: 'black',
-				list: {
-					background: 'yellow',
-					highlightColor: 'orange',
-					limit: 10,
-					showRows: 10
-				},
-				button: true
-		};
-		
-		// option setting
-		$.extend(option, options);
-		
+		var ulId = 'younjh_' + $this.attr("id") + '_ul';
+		var inputId = 'younjh_' + $this.attr("id") + '_input';
+		var showId = 'younjh_' + $this.attr("id") + '_show';
+		var defaultValue = $this.val();
+
+
 		makeLayout();
-		$younjh_input = $("#younjh_input");
-		$younjh_ul = $("#younjh_ul");
-		
+		$younjh_input = $("#" + inputId);
+		$younjh_ul = $("#" + ulId);
+
 		// style option application
 		style();
-		
+
 		// pre encoding
 		$.each($item, function(){
 			en.push(encodeURIComponent($(this).text()));
 			enIdx.push($(this).val());
 		});
-		
+
 		// event binding
 		binding();
 		
+		if(defaultValue != undefined){
+			$younjh_input.val($(this).children().filter('option[value=' + $(this).val() + ']').text());
+		}
+
 		function style(){
 			var inputWidth = 0;		// textbox객체 width
 			// 확장 버튼 
@@ -65,13 +77,13 @@
 			} else {
 				inputWidth = option.width - 6;
 			}
-			
+
 			$container.css({
 				width: option.width + 'px',
 				height: option.height + 'px',
 				border: option.border
 			});
-			
+
 			$younjh_input.css({
 				padding: '0px 3px',
 				width: inputWidth + 'px',
@@ -83,9 +95,9 @@
 				float: 'left',
 				border: '0px none'
 			});
-			
+
 			if(option.button){
-				$("#younjh_show").css({
+				$("#" + showId).css({
 					width: option.height + 'px',
 					height: option.height + 'px',
 					'font-size': option.fontSize,
@@ -97,50 +109,52 @@
 					cursor: 'pointer'
 				});
 			}
-			
+
 			$younjh_ul.css({
-				'margin-top': '20px',
+				'margin': '20px 0px 0px 0px',
 				'background': option.list.background,
-				border: '1px solid black',
-				display: 'none',
+				position: 'absolute',
+				width: (option.width-2) + 'px',
+				border: option.list.border,
 				height: (option.list.showRows * option.height) + 'px',
 				'overflow-y': 'auto',
 				'font-size': option.fontSize,
-				'font-family': option.fontFamily
+				'font-family': option.fontFamily,
+				display: 'none',
 			});
-			
-			 liStyle = 'padding: 0px 3px; height: 20px; line-height: 20px; overflow: hidden;';
-			 bStyle = 'color: ' + option.list.highlightColor;
+
+			liStyle = 'padding: 0px 3px; height: 20px; line-height: 20px; overflow: hidden;';
+			bStyle = 'color: ' + option.list.highlightColor;
 		}
-		
+
 		function makeLayout(){
 			var tag = '';
-			
+
 			$this.hide();
 			// container div
 			if(option.button){
-				tag = '<input type="text" id="younjh_input" value=""/><div id="younjh_show">▼</div><ul id="younjh_ul"></ul>';
+				tag = '<input type="text" id="' + inputId + '" value=""/><div id="' + showId + '">▼</div><ul id="' + ulId + '"></ul>';
 			} else {
-				tag = '<input type="text" id="younjh_input" value=""/><ul id="younjh_ul"></ul>';
+				tag = '<input type="text" id="' + inputId + '" value=""/><ul id="' + ulId + '"></ul>';
 			}
-			
+
 			$container.append(tag);
 		}
-		
+
 		function search(text, keyCode, isBtn){
 			var tmp = '';
 			result = "";
 			resultCnt = 0;
 			resultId = [];
-			
+
 			// textbox에 입력된 text를 encode
 			var esText = encodeURIComponent(text);
-			
+
 			// pre encode된 배열에서 입력된 text가 포함된 것을 찾는다.
 			for(var z=0; z<en.length; z++){
 				// 일치하는 부분에 하이라이트를 주기 위해 start index를 저장한다.
-				start = en[z].search(esText);
-				
+				start = en[z].indexOf(esText);
+
 				if (start > -1) {
 					// 하이라이트 표시 될 문자열
 					tmp = en[z].substring(start, start + (esText.length));
@@ -149,7 +163,7 @@
 					result += '<li style="' + liStyle + '">' + decodeURIComponent(en[z].replace(esText, '<b style="' + bStyle + '">' + tmp + '</b>')) + '</li>';
 					resultId.push(enIdx[z]);
 					resultCnt += 1;
-					
+
 					if(!(keyCode == 40 && text == "") && resultCnt == option.list.limit){
 						// textbox가 비어있는 상태에서 아래 방향키를 누른 경우는 전체 목록을 봐야 하므로 return을 하지 않고
 						// 그렇지 않은 경우에는 limit option의 값에 따라 break 한다.
@@ -157,9 +171,9 @@
 					}
 				}
 			}
-			
+
 			$younjh_ul.html(result);
-			
+
 			if(isBtn){
 				if($younjh_ul.css("display") == "none"){
 					$younjh_ul.css("display", "block");
@@ -170,12 +184,12 @@
 			} else {
 				$younjh_ul.css("display", "block");
 				ulBinding();
-				
+
 			}
-			
+
 			delayTimer = null;
 		}
-		
+
 		function ulBinding(){
 			$younjh_ul.children().bind("click", function(){
 				$younjh_input.val($(this).text());
@@ -187,20 +201,20 @@
 				$(this).removeClass("younjh-selected");
 			});
 		}
-		
+
 		function binding(){
 			// 마우스가 목록을 벗어나면 숨김
 			$younjh_ul.bind('mouseleave', function(){
 				liIdx = 0;
 				$(this).css("display", "none");
 			});
-			
+
 			// 검색어 입력
 			$younjh_input.keyup(function(){
 				var text = $younjh_input.val();
-				
+
 				keyCode = event.keyCode;
-				
+
 				if(keyCode == 40){
 					// 아래 방향키 : 40
 					// active 효과를 이동하면서 스크롤도 이동시킨다.
@@ -209,14 +223,14 @@
 						search(text, keyCode);
 						$younjh_ul.css("display", "block");
 					}
-					
+
 					if(liIdx < resultCnt){
 						$("li:nth-child(" + liIdx + ")", $younjh_ul).removeClass("younjh-selected");
 						$("li:nth-child(" + (++liIdx) + ")", $younjh_ul).addClass("younjh-selected");
 						if(liIdx > 10){
 							$younjh_ul.scrollTop((liIdx-option.list.showRows) * 20);
 						}
-						
+
 					}
 				} else if(keyCode == 38) {
 					// up key
@@ -228,20 +242,20 @@
 					}
 				} else {
 					var text = $younjh_input.val();
-					
+
 					if(keyCode == 8){
 						//backspace
 						liIdx = 0;
-						
+
 						if(text == ""){
 							$younjh_ul.css("display", "none");
 							return;
 						}
-						
+
 						search(text, keyCode, false);
 					} else if(keyCode == 13) {
 						// enter를 입력한 경우 방향키를 이용하여 active 상태가 된 li가 있다면 selectbox에 선택하고 textbox에 text를 보여준다.
-						var $selected = $("#younjh_ul li.younjh-selected");
+						var $selected = $("#" + ulId + " li.younjh-selected");
 						var v = $selected.index();
 
 						if(v > -1){
@@ -249,19 +263,19 @@
 							$this.val(resultId[$(this).index()]);
 							$younjh_ul.css("display", "none");
 						}
-						
+
 					} else {
 						search(text, keyCode, false);
 					}
 				}
 			});
-			
-			$("#younjh_show").click(function(){
+
+			$("#" + showId).click(function(){
 				var text = $younjh_input.val();
-				
+
 				search(text, keyCode, true);
 			});
 		}
-                return this;
+//		});
 	};
 })(jQuery);
