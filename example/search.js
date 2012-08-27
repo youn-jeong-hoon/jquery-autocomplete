649247bc51e4f1c11c5fe2507b43560983dcaa8b
youn-jeong-hoon <yjh5369@hanmail.net>
2012-08-27 11:27:04 +0900

    add example

---
+++ example/search.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#customer_id").younjhAutoComplete({
+		width: 150,
+		height: 20,
+		background: 'white',
+		button: false,
+		border: '0px none',
+		fontSize: 11,
+		limit: 20
+	});
+});
